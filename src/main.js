8b8da430dfeebedaa822c328dfbad1dd605331b3
April Sears <aprilnsears@gmail.com>
2026-05-23 17:51:40 -0400

    Restore style.css import and remove untracked vue-gtag-next dependency

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
 import { createApp } from 'vue'
-import VueGtag from 'vue-gtag-next'
 import router from './router'
 import App from './App.vue'
+import './style.css'
 
 const app = createApp(App)
-
 app.use(router)
-app.use(VueGtag, {
-  property: { id: import.meta.env.VITE_GA_ID }
-}, router)
-
 app.mount('#app')
