047a766dbbecc131a2efeedc1d6f09f6300006e7
April Sears <aprilnsears@gmail.com>
2026-05-23 16:44:27 -0400

    Analytic update

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 import { inject } from '@vercel/analytics'
-import VueGtag from 'vue-gtag'
 
 inject()
 
-const app = createApp(App)
-
-app.use(router)
-app.use(VueGtag, { property: { id: import.meta.env.VITE_GA_TRACKING_ID } }, router)
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
